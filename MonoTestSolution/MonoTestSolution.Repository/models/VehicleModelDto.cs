11eb468767787f5a703c5e75bf78c7b6894d4e61
Carlos <carlosokumu254@gmail.com>
2022-12-28 05:25:05 -0500

    [UPDATE]Changed Id Int to Guid

--- MonoTestSolution/MonoTestSolution.Repository/models/VehicleModelDto.cs
+++ MonoTestSolution/MonoTestSolution.Repository/models/VehicleModelDto.cs
@@ -6,7 +6,7 @@ namespace MonoTestSolution.Repository.models
 {
     public class VehicleModelDto
     {
-        public int Id { get; set; }
+        public int Guid { get; set; }
         public string Name { get; set; }
         public string Abbr { get; set; }
     }
