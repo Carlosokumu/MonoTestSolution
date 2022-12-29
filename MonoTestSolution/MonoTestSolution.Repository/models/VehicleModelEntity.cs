ce99d769fc39ee0aed0c2e36b29d84e7203ca5ce
Carlos <carlosokumu254@gmail.com>
2022-12-29 00:25:09 -0500

    [ADD]Added Vehicle Model Entity

---
+++ MonoTestSolution/MonoTestSolution.Repository/models/VehicleModelEntity.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoTestSolution.Repository.models
+{
+    public class VehicleModelEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [MaxLength(255)]
+        public string Abbr { get; set; }
+    }
+}
+
