84b4e030b68f38d35f15b1b1007ef80c78210b8d
Carlos <carlosokumu254@gmail.com>
2022-12-29 00:35:22 -0500

    [ADD]Registered Dependencies

---
+++ MonoTestSolution/MonoTestSolution/BootStrap/AppModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using MonoTestSolution.memory;
+using MonoTestSolution.Repository;
+using MonoTestSolution.Service;
+using MonoTestSolution.Service.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoTestSolution.BootStrap
+{
+     public class AppModule: Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<RepositoryDataSource>().AsSelf();
+            builder.RegisterType<VehicleMakeService>().As<IVehicleMakeService>();
+            builder.RegisterType<VehicleModelService>().As<IVehicleModelService>();
+        }
+
+    }
+}
