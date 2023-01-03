af721cb1efcc1c80f66ea102e663b2be5788aca1
Carlos <carlosokumu254@gmail.com>
2023-01-03 14:07:21 -0500

    [UPDATE]Code Refactoring

--- MonoTestSolution/MonoTestSolution/views/VehicleMakesListPage.xaml.cs
+++ MonoTestSolution/MonoTestSolution/views/VehicleMakesListPage.xaml.cs
@@ -4,6 +4,7 @@ using MonoTestSolution.interfaces;
 using MonoTestSolution.memory;
 using MonoTestSolution.Service.interfaces;
 using MonoTestSolution.viewmodels;
+using System;
 using Xamarin.Forms;
 
 namespace MonoTestSolution
@@ -18,7 +19,7 @@ namespace MonoTestSolution
             var vehicleMakeDetailsService = AppContainer.Container.Resolve<IvehicleMakeDetailsService>();
             var pageService = AppContainer.Container.Resolve<IpageService>();
 
-            ViewModel = new VehicleMakeListViewModel(vehicleMakeService,pageService,vehicleMakeDetailsService);
+            ViewModel = new VehicleMakeListViewModel(vehicleMakeService, pageService, vehicleMakeDetailsService);
             ViewModel.LoadDataCommand.Execute(null);
         }
 
@@ -32,6 +33,12 @@ namespace MonoTestSolution
         void OnMakeSelected(object sender, SelectedItemChangedEventArgs e)
         {
             ViewModel.SelectMakeCommand.Execute(e.SelectedItem);
+        }
+        void OnTextChanged(object sender, EventArgs e)
+        {
+            SearchBar searchBar = (SearchBar)sender;
+            ViewModel.SearchMake(searchBar.Text);
+
         }
     }
 }
