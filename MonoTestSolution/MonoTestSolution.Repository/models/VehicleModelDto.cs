c05e9805e682d63e6f0fb0584bc823a1d85993c0
Carlos <carlosokumu254@gmail.com>
2023-01-04 06:38:12 -0500

    [UPDATE]Added Year Field

--- MonoTestSolution/MonoTestSolution.Repository/models/VehicleModelDto.cs
+++ MonoTestSolution/MonoTestSolution.Repository/models/VehicleModelDto.cs
@@ -9,5 +9,6 @@ namespace MonoTestSolution.Repository.models
         public  Guid  Id { get; set; }
         public string Name { get; set; }
         public string Abbr { get; set; }
+        public string Year { get; set; }
     }
 }
