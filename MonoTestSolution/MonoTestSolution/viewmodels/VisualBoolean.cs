8ced32824afdb035be7884200601e89bf3abe68d
Carlos <carlosokumu254@gmail.com>
2023-01-04 06:36:19 -0500

    [UPDATE]Added Class to Help Handle Loading

---
+++ MonoTestSolution/MonoTestSolution/viewmodels/VisualBoolean.cs
@@ -0,0 +1,22 @@
+﻿namespace MonoTestSolution.viewmodels
+{
+    public class VisualBoolean: BaseViewModel
+    {
+
+        public VisualBoolean()
+        {
+
+        }
+        public bool _isLoading;
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set
+            {
+                SetValue(ref _isLoading, value);
+                OnPropertyChanged(nameof(IsLoading));
+            }
+        }
+    }
+    
+}
